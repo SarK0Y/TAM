07620eefaa171389cd82064988dbf25e443d6564
SarK0Y <z0dchiy8@gmail.com>
2023-06-25 20:57:18 +0300

    Update taw.ps1

--- taw.ps1
+++ taw.ps1
@@ -1,3 +1,3 @@
 Set-PSDebug -Trace 2
 Set-Alias -Name grep -Value findstr
-py.exe Z:\taw0.py  -find_files -path0 "S:\" -tmp_file "C:\Users\tst\tmp\01547.log"  -view_w "C:\Program Files\VideoLAN\VLC\vlc.exe" "--sout-x264-b-bias=-15" -view_w "C:\Program Files\SMPlayer\smplayer.exe" $args
+py.exe Z:\taw.py  -find_files -path0 "S:\" -tmp_file "C:\Users\tst\tmp\01547.log"  -view_w "C:\Program Files\VideoLAN\VLC\vlc.exe" "--sout-x264-b-bias=-15" -view_w "C:\Program Files\SMPlayer\smplayer.exe" $args
