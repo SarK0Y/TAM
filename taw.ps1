366dce22539e5c8dba9a1478891ddcc012c9845f
SarK0Y <z0dchiy8@gmail.com>
2023-06-26 20:04:15 +0300

    powershell taw.ps1

---
+++ taw.ps1
@@ -0,0 +1,1 @@
+py.exe Z:\taw.py  -find_files -path0 "S:\" -tmp_file "C:\Users\tst\tmp\01547.log"  -view_w "C:\Program Files\VideoLAN\VLC\vlc.exe" "--sout-x264-b-bias=-15" -view_w "C:\Program Files\SMPlayer\smplayer.exe" $args
