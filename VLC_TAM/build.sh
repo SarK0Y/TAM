eb2cd189f62fa82fabf56359ffedbb537b73e1bd
SarK0Y <z0dchiy8@gmail.com>
2023-08-05 17:17:54 +0300

    Delete build.sh

--- VLC_TAM/build.sh
+++
@@ -1,3 +0,0 @@
-clear;make -f Makefile0 clean;make -f Makefile0 objs --eval="EXTRA_INCLUDES=/usr/include/vlc/plugins" --eval="VLC_INCLUDES=/home/evg/src/vlc-3.0.18/include/" \
---eval="QT5_INCLUDES=/usr/include/x86_64-linux-gnu/qt5/" \
---eval="dotHPPs=/home/evg/src/vlc-3.0.18/modules/gui/qt/"; make -f Makefile0
