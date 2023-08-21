61e5836750ca534f9986f0729daba35d647c5bd6
SarK0Y <z0dchiy8@gmail.com>
2023-08-21 21:11:03 +0300

    looks working but i do like more tests :)

--- tam.py
+++ tam.py
@@ -61,19 +61,30 @@ class lapse:
     read_midway_data_from_pipes_stop = 0
 class var_4_hotKeys:
     prnt: str
+    promt: str
+    save_promt_to_copy_file: str
+    save_prnt_to_copy_file: str
     prnt_short: str
     prnt_full: str
     fileName: str
+    copyFile_msg: str
     fileIndx: int
     full_length: int
+    ENTER_MODE = False
 # Terminals
 def handleENTER(fileName: str) -> str:
+    var_4_hotKeys.ENTER_MODE = True
     if var_4_hotKeys.prnt[:3] == 'ren':
+        var_4_hotKeys.save_prnt_to_copy_file = var_4_hotKeys.prnt
+        var_4_hotKeys.save_promt_to_copy_file = var_4_hotKeys.promt
         renameFile(fileName, var_4_hotKeys.prnt)
+        var_4_hotKeys.prnt = var_4_hotKeys.save_prnt_to_copy_file
+        var_4_hotKeys.promt = var_4_hotKeys.save_promt_to_copy_file
+        var_4_hotKeys.ENTER_MODE = False
         return f"go2 {page_struct.num_page}"
     if var_4_hotKeys.prnt[:2] == "cp":
         if os.path.isfile(getFileNameFromCMD(var_4_hotKeys.prnt)):
-            copy_file_msg = f"Do You really want to overwrite {getFileNameFromCMD(var_4_hotKeys.prnt)} ??? Type 'Yeah I do' if You {Fore.RED}{Back.BLACK}REALLY{Style.RESET_ALL} do.. Otherwise just 'no'. "
+            var_4_hotKeys.copyFile_msg = f"Do You really want to overwrite {getFileNameFromCMD(var_4_hotKeys.prnt)} ??? Type 'Yeah I do' if You {Fore.RED}{Back.BLACK}REALLY{Style.RESET_ALL} do.. Otherwise just 'no'. "
             if var_4_hotKeys.save_prnt_to_copy_file == '':
                 var_4_hotKeys.save_prnt_to_copy_file = var_4_hotKeys.prnt
                 var_4_hotKeys.prnt = ""
@@ -85,25 +96,30 @@ def handleENTER(fileName: str) -> str:
                 writeInput_str(var_4_hotKeys.copyFile_msg, var_4_hotKeys.prnt, full_length)
                 return "cont"
         else:
-            copyFile(fileName, prnt)
+            copyFile(fileName, var_4_hotKeys.prnt)
+            var_4_hotKeys.prnt = var_4_hotKeys.save_prnt_to_copy_file
+            var_4_hotKeys.promt = var_4_hotKeys.save_promt_to_copy_file
+            var_4_hotKeys.ENTER_MODE = False
             return f"go2 {page_struct.num_page}"
     if var_4_hotKeys.prnt == "Yeah I do":
         var_4_hotKeys.promt = ' ' * len(var_4_hotKeys.promt)
-        prnt = ' ' * len(prnt)
-        writeInput_str(promt, prnt)
-        prnt = save_prnt_to_copy_file
-        promt = save_promt_to_copy_file
-        save_promt_to_copy_file = save_prnt_to_copy_file = ''
-        copyFile(fileName, prnt, dontInsert=True)
-        writeInput_str(promt, prnt)
-        continue
-    if prnt == "no":
-        prnt = save_prnt_to_copy_file
-        promt = save_promt_to_copy_file
-        save_promt_to_copy_file = ''
-        save_prnt_to_copy_file = ''
-        writeInput_str(promt, prnt)
+        prnt = ' ' * len(var_4_hotKeys.prnt)
+        writeInput_str(var_4_hotKeys.promt, var_4_hotKeys.prnt)
+        var_4_hotKeys.prnt = var_4_hotKeys.save_prnt_to_copy_file
+        var_4_hotKeys.promt = var_4_hotKeys.save_promt_to_copy_file
+        var_4_hotKeys.save_promt_to_copy_file = var_4_hotKeys.save_prnt_to_copy_file = ''
+        copyFile(fileName, var_4_hotKeys.prnt, dontInsert=True)
+        writeInput_str(var_4_hotKeys.promt, var_4_hotKeys.prnt)
+        return "cont"
+    if var_4_hotKeys.prnt == "no":
+        var_4_hotKeys.prnt = var_4_hotKeys.save_prnt_to_copy_file
+        var_4_hotKeys.promt = var_4_hotKeys.save_promt_to_copy_file
+        var_4_hotKeys.save_promt_to_copy_file = ''
+        var_4_hotKeys.save_prnt_to_copy_file = ''
+        writeInput_str(var_4_hotKeys.promt, var_4_hotKeys.prnt)
+        var_4_hotKeys.ENTER_MODE = False
         return f"go2 {page_struct.num_page}"
+    return var_4_hotKeys.prnt
 def handleTAB(promt: str):
     ptrn = re.compile('ren\s+\d+|cp\s+\d+', re.IGNORECASE | re.UNICODE)
     regex_result = ptrn.search(var_4_hotKeys.prnt)
@@ -267,9 +283,9 @@ def ord0(Key):
 def hotKeys(promt: str) -> str:
     full_length = 0
     prnt = ""
-    save_prnt_to_copy_file = ''
-    save_promt_to_copy_file = ''
-    save_cur_cur_pos = page_struct.cur_cur_pos
+    var_4_hotKeys.save_prnt_to_copy_file = ''
+    var_4_hotKeys.save_promt_to_copy_file = ''
+    var_4_hotKeys.save_cur_cur_pos = page_struct.cur_cur_pos
     prnt0 = ''
     prnt_short = ''
     prnt_full = ''
@@ -311,42 +327,14 @@ def hotKeys(promt: str) -> str:
                 print('\033[D', end='', flush=True)
             continue
         if ENTER == ord0(Key):
-            if prnt[:3] == 'ren':
-                renameFile(fileName, prnt)
-                return f"go2 {page_struct.num_page}"
-            if prnt[:2] == "cp":
-                if os.path.isfile(getFileNameFromCMD(prnt)):
-                    copy_file_msg = f"Do You really want to overwrite {getFileNameFromCMD(prnt)} ??? Type 'Yeah I do' if You {Fore.RED}{Back.BLACK}REALLY{Style.RESET_ALL} do.. Otherwise just 'no'. "
-                    if save_prnt_to_copy_file == '':
-                        save_prnt_to_copy_file = prnt
-                        prnt = ""
-                        save_promt_to_copy_file = promt
-                        promt = copy_file_msg
-                        full_length = len(copy_file_msg) + len(prnt)
-                        page_struct.left_shift_4_cur = 0
-                        page_struct.cur_cur_pos = full_length
-                        writeInput_str(copy_file_msg, prnt, full_length)
-                        continue
-                else:
-                    copyFile(fileName, prnt)
-                    return f"go2 {page_struct.num_page}"
-            if prnt == "Yeah I do":
-                promt = ' ' * len(promt)
-                prnt = ' ' * len(prnt)
-                writeInput_str(promt, prnt)
-                prnt = save_prnt_to_copy_file
-                promt = save_promt_to_copy_file
-                save_promt_to_copy_file = save_prnt_to_copy_file = ''
-                copyFile(fileName, prnt, dontInsert=True)
-                writeInput_str(promt, prnt)
+            if not var_4_hotKeys.ENTER_MODE:
+                var_4_hotKeys.prnt = prnt
+                var_4_hotKeys.promt = promt
+                ret = handleENTER(fileName)
+            if "cont" == ret:
                 continue
-            if prnt == "no":
-                prnt = save_prnt_to_copy_file
-                promt = save_promt_to_copy_file
-                save_promt_to_copy_file = ''
-                save_prnt_to_copy_file = ''
-                writeInput_str(promt, prnt)
-                return f"go2 {page_struct.num_page}"
+            promt = var_4_hotKeys.save_promt_to_copy_file
+            prnt = ret
             return prnt
         if DELETE == Key:
             if page_struct.left_shift_4_cur == 0:
